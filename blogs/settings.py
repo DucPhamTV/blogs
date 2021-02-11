7dd41b6d2b1a3d8e1cecbde82eb05330abe46ddb
DucPhamTV <phamcongduc1994@gmail.com>
2021-02-11 08:57:38 +0000

    Update hltm files. Add django-on-heroku to settings.py

--- blogs/settings.py
+++ blogs/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import django_on_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,3 +126,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+django_on_heroku.settings(locals())
